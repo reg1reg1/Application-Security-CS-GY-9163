d0eb7495ef7e8e520ac78ccdd96dba79ea67994c
Yuvraj Rajeev Singh <ys3334@nyu.edu>
2019-09-25 10:15:17 -0400

    Preparing for make

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include "spell.h"
+
+
+void main(int argc, char *argv[] )
+{	
+	if( argc == 3 ) {
+      
+
+      printf("The argument supplied is %s %s\n", argv[1],argv[2]);
+   	
+
+   	}
+
+
+	return ;
+}
