74e8828bc7045faf67ceb18a8393106fb6ec0ab3
Yuvraj Rajeev Singh <ys3334@nyu.edu>
2019-09-30 01:05:21 -0400

    Modifying main to check autograder

--- main.c
+++ main.c
@@ -1,66 +1,30 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
+#include <string.h>
 
-#include <ctype.h>
 #include "dictionary.h"
-
-//global variable declaration to reduce heap memory space
-
-
-int main(int argc, char *argv[] )
-{	
-	node* hashtable[HASH_SIZE];
-	char* misspelled[MAX_MISSPELLED];
-	bool status=false;
-	int misspell_count=0;
-	
-	//Usage print
-	if (argc!=3)
-	{
-		fprintf(stdout,"Usage incorrect!\n");
-		fprintf(stdout,"./spell_check <dictionary.txt> <file_to_be.txt>\n");
-		
-	}
-	else
-	{	
-		const char *dictionary = argv[2];
-		const char *inputText = argv[1];
-		
-		fprintf(stdout,"%s %s\n",dictionary,inputText);
-		status=load_dictionary(dictionary,hashtable);
-		if(!status)
-		{
-			fprintf(stdout,"Could not load dictionary file,...\n Aborting ...\n");
-			return 0;
-		}
-		FILE *fp = fopen(inputText,"r");
-		if(fp == NULL) {
-         perror("Unable to open file!");
-          exit(1);
-     	}
-     	misspell_count= check_words(fp,hashtable,misspelled);
-     	//fclose(fp);
-     	if(misspell_count>0)
-     	{	 fprintf(stdout,"\n\n\n\n**************%d Mispelled words***************\n",misspell_count);
-
-     		 print_mispelled(misspelled,misspell_count);
-     		 fprintf(stdout,"\n\n\n\n************************************************\n");
-     	}
-     	else
-     	{
-     		fprintf(stdout,"All words are spelled correctly!\n");
-     	}
-     	
-     	
-
-	}
-	exit(0);
+#undef calculate
+#undef getrusage
+#define expected_size 118819
+// default dictionary
+#define DICTIONARY "wordlist.txt"
+#define BODICTIONARY "bo_wordlist.txt"
+#define MAX_MISSPELLED 1000
+int main(int argc, char** argv) {
+    hashmap_t hashtable[HASH_SIZE];
+    if (argc < 3) {
+        fprintf(stderr, "Error: Insufficient arguments!\n");
+        fprintf(stderr, "Usage: ./program to_check.txt wordlist.txt\n");
+        exit(-1);
+    }
+    char read_mode[2] = "r";
+    FILE* fp = fopen(argv[1], read_mode);
+    char * dictionary = argv[2]; 
+    load_dictionary(dictionary, hashtable);
+    char* misspelled[MAX_MISSPELLED];
+    int num_wrong = check_words(fp, hashtable, misspelled);
+    for (int i = 0; i < num_wrong; i++) {
+        printf("%s\n", misspelled[i]);
+    }
 }
-
-/** Commands to execute
-gcc -Wall -c dictionary.c dictionary.h
-gcc -Wall -c spell.c
-gcc -Wall -c main.c
-gcc -Wall -o spell_check dictionary.o spell.o main.o
-**/
