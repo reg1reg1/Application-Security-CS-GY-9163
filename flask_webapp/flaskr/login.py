80e1ce156d6adc7a92960c2b87134eaabd6851e4
Yuvraj Singh <ys3334@nyu.edu>
2019-10-21 21:18:19 -0400

    Adding flask folder, testing verified commit from different machine

---
+++ flask_webapp/flaskr/login.py
@@ -0,0 +1,89 @@
+ 
+import functools
+
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from flaskr.db import get_db
+
+#rootview
+root_view = Blueprint('main', __name__, url_prefix='/')
+
+@root_view.route('/register', methods=('GET', 'POST'))
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+
+        if not username:
+            error = 'Empty username'
+        elif not password:
+            error = 'Empty password'
+        elif db.execute(
+            'SELECT id FROM user WHERE username = ?', (username,)
+        ).fetchone() is not None:
+            error = 'User {} is already registered.'.format(username)
+
+        if error is None:
+            db.execute(
+                'INSERT INTO user (username, password) VALUES (?, ?)',
+                (username, generate_password_hash(password))
+            )
+            db.commit()
+            return redirect(url_for('login.login'))
+
+        flash(error)
+
+    return render_template('/register.html')
+
+@root_view.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
+
+
+@root_view.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+        #Remove later, chances of sqli
+        user = db.execute(
+            'SELECT * FROM user WHERE username = ?', (username,)
+        ).fetchone()
+
+        if user is None:
+            error = 'Incorrect username.'
+        elif not check_password_hash(user['password'], password):
+            error = 'Incorrect password.'
+
+        if error is None:
+            session.clear()
+            session['user_id'] = user['id']
+            return redirect(url_for('index'))
+
+        flash(error)
+
+    return render_template('/login.html')        
+
+
+#mapping the session to an existing user in the database.
+@root_view.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+            'SELECT * FROM user WHERE id = ?', (user_id,)
+        ).fetchone()
+
+
+
